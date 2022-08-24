746ff1b67c8ff0e73b5c775a4cdf2e58e8b0026e
Châu Thái Nhân <ctnhan044@gmail.com>
2022-08-24 10:49:05 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,14 +1,7 @@
 from flask import Flask, Response
-from google_play_scraper import app
-import json
 
 app = Flask(__name__)
  
 @app.route('/')
 def home():
     return 'You are at home page.'
-    
-@app.route('/apps/<path:appid>')
-def apps(appid):
-    result = app(appid, lang='en', country='us')
-    return Response(json.dumps(result), mimetype="application/json")
