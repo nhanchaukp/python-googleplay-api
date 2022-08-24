c9746be0c8905db84ae9da65a1dd2ce8c5fb8635
Châu Thái Nhân <ctnhan044@gmail.com>
2022-08-24 10:14:38 +0700

    Update app.py

--- api/app.py
+++ api/app.py
@@ -5,14 +5,10 @@ import json, os
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         spath = os.path.split(self.path)
-        print(spath)
-        try:
-            if spath[0]=='/app':
-                result = app(spath[1], lang='en', country='us')
-                self.send_response(200)
-                self.send_header("Content-type", "application/json")
-                self.end_headers()
-                self.wfile.write(bytes(json.dumps(result), 'utf8'))
-                return
-        except:
-            self.send_error(500, "Error")
+        if spath[0]=='/app':
+            result = app(spath[1], lang='en', country='us')
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(bytes(json.dumps(result), 'utf8'))
+            return
